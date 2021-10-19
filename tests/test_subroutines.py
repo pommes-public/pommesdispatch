c3fc19bbeef3f85fc251cbb60e26e77b85ad4167
Johannes Kochems <jokochems@web.de>
2021-10-19 09:31:56 +0200

    Add further tests for subroutines

--- tests/test_subroutines.py
+++ tests/test_subroutines.py
@@ -78,5 +78,112 @@ class TestSubroutines:
         assert len(node_dict) == 10
         assert type(node_dict["DE_source_hardcoal"]) == oemof.solph.Source
         assert node_dict["DE_source_hardcoal"].outputs[
-                 node_dict["DE_bus_hardcoal"]].variable_costs.default == (
-            17.853)
+                   node_dict["DE_bus_hardcoal"]].variable_costs.default == (
+                   17.853)
+
+    def test_create_shortage_sources(self):
+        """test function create_shortage_sources"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_shortage_sources(
+            input_data, node_dict)
+
+        assert len(node_dict) == 7
+        assert type(node_dict["DE_source_el_shortage"]) == oemof.solph.Source
+        assert node_dict["DE_source_el_shortage"].outputs[
+                   node_dict["DE_bus_el"]
+               ].variable_costs.default == 160
+
+    def test_create_renewables(self):
+        """test function create_renewables"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_renewables(
+            input_data, dm, node_dict)
+
+        assert len(node_dict) == 7
+        assert type(node_dict["DE_source_biomassEEG"]) == oemof.solph.Source
+        assert node_dict["DE_source_biomassEEG"].outputs[
+                   node_dict["DE_bus_el"]
+               ].variable_costs.default == 0
+        assert len(node_dict["DE_source_biomassEEG"].outputs[
+                       node_dict["DE_bus_el"]
+                   ].fix) == 2
+        assert node_dict["DE_source_biomassEEG"].outputs[
+                   node_dict["DE_bus_el"]
+               ].nominal_value == 7263.8
+
+    def test_create_demand(self):
+        """test function create_demand"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_demand(
+            input_data, dm, node_dict)
+
+        assert len(node_dict) == 7
+        assert type(node_dict["DE_sink_el_load"]) == oemof.solph.Sink
+        assert node_dict["DE_sink_el_load"].inputs[
+                   node_dict["DE_bus_el"]
+               ].nominal_value == 79063
+        assert len(node_dict["DE_sink_el_load"].inputs[
+                       node_dict["DE_bus_el"]
+                   ].fix) == 2
+
+    def test_create_excess_sinks(self):
+        """test function create_excess_sinks"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_excess_sinks(
+            input_data, node_dict)
+
+        assert len(node_dict) == 7
+        assert type(node_dict["DE_sink_el_excess"]) == oemof.solph.Sink
+        keys = ["DE_bus_solarPV", "DE_bus_windonshore", "DE_bus_windoffshore"]
+        for key in keys:
+            assert (node_dict["DE_sink_el_excess"].inputs[node_dict[key]]
+                    is not None)
+
+    # TODO: Include further assert statements and check parameterization
+    def test_create_transformers_conventional(self):
+        """test function create_transformers_conventional"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_transformers_conventional(
+            input_data, dm, node_dict)
+
+        assert len(node_dict) == 9
+
+    def test_create_transformers_res(self):
+        """test function create_transformers_res"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_transformers_res(
+            input_data, dm, node_dict)
+
+        assert len(node_dict) == 12
+
+    def test_create_storages(self):
+        """test function create_storages"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        node_dict = subroutines.create_storages(
+            input_data, dm, node_dict)
+
+        assert len(node_dict) == 7
+
+    def test_create_storages_rolling_horizon(self):
+        """test function create_storages_rolling_horizon"""
+        input_data, dm, node_dict = create_inputs_model_and_nodes()
+        node_dict = subroutines.create_buses(input_data, node_dict)
+        iteration_results = {
+            "storages_initial": pd.DataFrame(),
+            "model_results": {},
+            "dispatch_results": pd.DataFrame(),
+            "power_prices": pd.DataFrame()
+        }
+        node_dict, storage_labels = (
+            subroutines.create_storages_rolling_horizon(
+                input_data, dm, node_dict, iteration_results))
+
+        assert len(node_dict) == 7
+        assert storage_labels == ["DE_storage_el_PHS"]
